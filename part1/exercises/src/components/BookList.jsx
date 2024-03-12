db5dec85d44a317b5f9a185e4e5f9670abaf85e1
Travis <azai392@gmail.com>
2024-03-11 20:36:03 -0500

    Part 1 Done

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Great Books!";
+   let book1 = "https://m.media-amazon.com/images/I/71f32nD8PCL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://cdn.kobo.com/book-images/20b5525d-668b-43c7-9049-40244fd6a8f9/1200/1200/False/the-count-of-monte-cristo-342.jpg";
+   let book3 = "https://i0.wp.com/jonathanpongratz.com/wp-content/uploads/2020/04/animorphs29.jpg?fit=318%2C459&ssl=1";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="World War Z by Max Brooks" />
+         <img src={book2} alt="The Count of Monte Cristo by Alexandre Dumas" />
+         <img src={book3} alt="Animorphs #29 - The Sickness by K.A. Applegate" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,17 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from 'react';
+import classes from './ChoresList.module.css';
+
+const ChoresList = () => {
+   return <div>
+      <h3 className = {classes.choresHeading}>Today's Chores</h3>
+         <p>
+            <ul>
+               <li className = {classes.choresText}>Wash Dishes</li>
+               <li className = {classes.choresText}>Sweep Stairs</li>
+            </ul>
+         </p>
+      </div>;
 }
+
+
+export default ChoresList;
