9533011cb3a786fdb2e9e731632a041085da849a
Travis <azai392@gmail.com>
2024-03-18 19:10:48 -0500

    Part 3 Exercise done!

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Mini Painting</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Always thin your paint down with water to apply an even coat and work your way up to the shade you want.</li>
+            <li>A smaller brush doesn't always make painting details easier.</li>
+            <li>A Base coat of a solid color like black or white depending on the tone you want to go with will help a lot with shading down the line.</li>
          </ol>
       </div>
    );
